03f3debd484618f5be25e67ec60763175196d8eb
king-shubham <kumarshu9412@gmail.com>
2026-01-05 20:01:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ load_dotenv()
 
 class Config:
     # Telegram API credentials (from https://my.telegram.org)
-    API_ID = int(os.getenv('API_ID', 12345678))
-    API_HASH = os.getenv('API_HASH', 'your_api_hash_here')
+    API_ID = int(os.getenv('API_ID', 35415101))
+    API_HASH = os.getenv('API_HASH', 'ab68651b9cb3c6757baf547cabef27d6')
     
     # Your Telegram account credentials
-    PHONE_NUMBER = os.getenv('PHONE_NUMBER', '+1234567890')
+    PHONE_NUMBER = os.getenv('PHONE_NUMBER', '+918859679570')
     
     # Session settings
     SESSION_NAME = 'fam_bot_session'
